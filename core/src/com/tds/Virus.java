6e7ab0bc8fedc83b153565e7cecb761e40bcca9b
Matthew Brown <matt2714@vandals.uidaho.edu>
2016-03-26 12:39:40 -0700

    Added Classes

---
+++ core/src/com/tds/Virus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tds;
+
+import com.tds.Intity;
+/**
+ *
+ * @author mattb
+ */
+public class Virus extends Intity{
+
+    public Virus(float health, float speed) {
+        super(health, speed);
+    }
+    
+}
